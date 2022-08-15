4aa1b4e676ff0a72252236ebc3cf243c24ccd3c0
n-rincon4 <n.rincon4@uniandes.edu.co>
2022-08-14 22:42:58 -0500

    Versión final - Taller 1 DPOO

---
+++ Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,22 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modificacion 
+{
+	// ************************************************************************
+	// CAMBIO AGREGADO - Método Main
+	// CAMBIO AGREGADO - Arreglo automático de Eclipse (IOException)
+	// ************************************************************************
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException
+	{
+		System.out.println("Hola, mundo!");
+		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
+	}
+}
